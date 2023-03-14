2cc430fa4709a86fb5c9b6a0d5a8bc3c7c88074d
Kisaragi Marine <kisaragi.effective@gmail.com>
2023-03-14 18:06:43 +0900

    perf: cache intermediate compile

--- scripts/compile_intermediate.sh
+++ scripts/compile_intermediate.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 out_dir="$(dirname "$0")/../intermediate"
+cache_file="$out_dir/upstream-commit.txt"
+
+function upstream() {
+  git rev-parse HEAD:original
+}
+
+if [ -f "$cache_file" ]; then
+  commit="$(tr -d '\n' < "$cache_file")"
+  if [ "$commit" == "$(upstream)" ]; then
+    echo "Fresh: original" >&2
+    exit 0
+  else
+    rm "$cache_file"
+    printf "%s" "$(upstream)" > "$cache_file"
+  fi
+else
+  printf "%s" "$(upstream)" > "$cache_file"
+fi
+
 [ ! -d "$out_dir" ] && mkdir "$out_dir"
 
 yq < original/lists/minecraft.yaml \
